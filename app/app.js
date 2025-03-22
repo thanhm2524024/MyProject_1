7794abbac492e5c467845b8b7da5e006a3cea6ff
Thanhm2524024 <thanhm2524024@gstudent.ctu.edu.vn>
2025-03-22 09:24:25 +0700

    update web

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
+    // Đọc file index.html
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Lỗi server!");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        }
+    });
 });
 
 server.listen(port, () => {
-  console.log(`Server running at port ` + port);
+    console.log(`Server running at http://localhost:${port}`);
 });
